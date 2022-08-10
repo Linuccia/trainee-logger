1423d7a4523111d242a66691de3f5638cc7cedc1
Linuccia <polina.stephanskaya@gmail.com>
2022-08-10 21:58:23 +0300

    initial implementation

--- src/main/java/aston/polina/config/Config.java
+++ src/main/java/aston/polina/config/Config.java
@@ -2,15 +2,12 @@ package aston.polina.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(value = "aston.polina")
-//@PropertySource(value = "classpath:application.properties")
 public class Config implements WebMvcConfigurer {
 
 }
